867438fae8542e3e7a4426e66d406fbdcb1b4d1d
kazimustafa95 <kazi.mustafa95@gmail.com>
2024-02-25 02:49:49 +0500

    update

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createPost } from '../controllers/post.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
+import { getPosts } from '../controllers/post.controller.js';
 
 
 
@@ -11,7 +12,7 @@ import { verifyToken } from '../utils/verifyUser.js';
 const postRoutes = express.Router();
 
 postRoutes.post('/create', verifyToken, createPost);
-
+postRoutes.get('/get',  getPosts);
 
 
 
--- frontend/src/components/DashPosts.jsx
+++ frontend/src/components/DashPosts.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 
 const DashPosts = () => {
-  return (
-    <div>DashPosts</div>
-  )
-}
+  const { currentUser } = useSelector((state) => state.user);
+  const [posts, setPosts] = useState([]);
 
-export default DashPosts
+  useEffect(() => {
+    const getPosts = async () => {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+      const data = await res.json();
+      setPosts(data);
+    };
+    getPosts();
+  }, [currentUser._id]);
+
+  console.log(posts);
+
+  return (
+    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500"></div>
+  );
+};
+
+export default DashPosts;
